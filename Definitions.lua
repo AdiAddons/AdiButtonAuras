4bc45897fa8a60f65fd643c60d77eac05b9bc791
Adirelle <adirelle@gmail.com>
2013-10-30 17:30:08 +0100

    Added support for interrupts.

--- Definitions.lua
+++ Definitions.lua
@@ -6,9 +6,80 @@ All rights reserved.
 
 local addonName, addon = ...
 
+local _G = _G
+local UnitCanAttack = _G.UnitCanAttack
+local UnitCastingInfo = _G.UnitCastingInfo
+local UnitChannelInfo = _G.UnitChannelInfo
+
 function addon.CreateRules()
+
 	addon:Debug('Creating Rules')
 	return {
+
+		-- Interrupts: use a custom configuration
+		Configure(
+			{ -- Spells
+				-- Deathknight
+				 47476, -- Strangulate
+				 47528, -- Mind Freeze
+				 91802, -- Shambling Rush (Ghoul)
+				-- Druid
+				 78675, -- Solar Beam
+				 80964, -- Skull Bash (bear)
+				 80965, -- Skull Bash (cat)
+				-- Hunter
+				 26090, -- Pummel (Gorilla)
+				 34490, -- Silencing Shot
+				 50318, -- Serenity Dust (Moth)
+				 50479, -- Nether Shock (Nether Ray)
+				147362, -- Counter Shot
+				-- Mage
+				  2139, -- Counterspell
+				-- Monk
+				116705, -- Spear Hand Strike
+				-- Paladin
+				 96231, -- Rebuke
+				-- Priest
+				 15487, -- Silence
+				-- Rogue
+				  1766, -- Kick
+				-- Shaman
+				 57994, -- Wind Shear
+				-- Warlock
+				 19647, -- Spell Lock (Felhunter)
+				103967, -- Carrion Swarm (demon form)
+				119911, -- Optical Blast (Observer special ability)
+				-- Warrior
+				  6552, -- Pummel
+			},
+			-- Unit
+			"enemy",
+			{ -- Events
+				"UNIT_SPELLCAST_CHANNEL_START",
+				"UNIT_SPELLCAST_CHANNEL_STOP",
+				"UNIT_SPELLCAST_CHANNEL_UPDATE",
+				"UNIT_SPELLCAST_DELAYED",
+				"UNIT_SPELLCAST_INTERRUPTIBLE",
+				"UNIT_SPELLCAST_NOT_INTERRUPTIBLE",
+				"UNIT_SPELLCAST_START",
+				"UNIT_SPELLCAST_STOP",
+			},
+			-- Handler
+			function(unit, model)
+				if UnitCanAttack("player", unit) then
+					local name, _, _, _, _, endTime, _, _, notInterruptible = UnitCastingInfo(unit)
+					if name and not notInterruptible then
+						model.highlight, model.expiration = "flash", endTime / 1000
+					end
+					name, _, _, _, _, endTime, _, notInterruptible = UnitChannelInfo(unit)
+					if name and not notInterruptible then
+						model.highlight, model.expiration = "flash", endTime / 1000
+					end
+				end
+			end
+		), -- Interrupts
+
+		-- Hunter spells
 		IfClass { "HUNTER",
 			SimpleBuffs {
 				 53271, -- Master's Call
