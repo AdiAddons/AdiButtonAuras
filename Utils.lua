dee73d7248d92007a5df1ef615644881c1f5b147
Adirelle <adirelle@gmail.com>
2013-10-30 11:57:47 +0100

    Added a "getkeys" utility function.

--- Utils.lua
+++ Utils.lua
@@ -6,10 +6,24 @@ All rights reserved.
 
 local addonName, addon = ...
 
-function addon.Memoize(func)
+local _G = _G
+local next = _G.next
+local setmetatable = _G.setmetatable
+local tostring = _G.tostring
+
+local function Memoize(func)
 	return setmetatable({}, {__index = function(self, key)
 		local value = func(key)
 		self[key] = value
 		return value
 	end})
 end
+addon.Memoize = Memoize
+
+local function getkeys(t, prevKey)
+	local key = next(t, prevKey)
+	if key then
+		return tostring(key), getkeys(t, key)
+	end
+end
+addon.getkeys = getkeys
