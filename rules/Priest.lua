a1174a443d9ce6e201d36b0ff7111a535c3ebd23
Adirelle <adirelle@gmail.com>
2013-12-01 18:56:18 +0100

    Added the rule stubs for priests and death knights.
    Fixes #5.

---
+++ rules/Priest.lua
@@ -0,0 +1,36 @@
+--[[
+AdiButtonAuras - Display auras on action buttons.
+Copyright 2013 Adirelle (adirelle@gmail.com)
+All rights reserved.
+
+This file is part of AdiButtonAuras.
+
+AdiButtonAuras is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+AdiButtonAuras is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with AdiButtonAuras.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+
+if select(2, UnitClass("player")) ~= "PRIEST" then return end
+
+local addonName, addon = ...
+
+-- Globals: AddRuleFor Configure IfSpell SimpleAuras UnitBuffs
+-- Globals: PassiveModifier SimpleDebuffs SharedSimpleDebuffs SimpleBuffs
+-- Globals: LongestDebuffOf SelfBuffs PetBuffs BuffAliases DebuffAliases
+-- Globals: SelfBuffAliases SharedBuffs ShowPower SharedSimpleBuffs
+-- Globals: BuildAuraHandler_Longest ImportPlayerSpells bit BuildAuraHandler_Single
+-- Globals: math
+
+AdiButtonAuras:RegisterRules(function(addon)
+	addon.Debug('Rules', 'Adding priest rules')
+	return ImportPlayerSpells { "PRIEST" }
+end)
