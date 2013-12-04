67290c9318d57eef0b7c9a522fd95ca5c46d296d
Adirelle <adirelle@gmail.com>
2013-12-04 11:59:09 +0100

    Monk: merged the Mistweaver plugin into base monk rules.
    There is almost no gain to keep it separated.

--- rules/Monk.lua
+++ rules/Monk.lua
@@ -33,6 +33,32 @@ AdiButtonAuras:RegisterRules(function(addon)
 
 	local L = addon.L
 
+	local ceil = _G.ceil
+	local floor = _G.floor
+	local format = _G.format
+	local GetNumGroupMembers = _G.GetNumGroupMembers
+	local GetSpellBonusHealing = _G.GetSpellBonusHealing
+	local GetSpellInfo = _G.GetSpellInfo
+	local GetTime = _G.GetTime
+	local min = _G.min
+	local pairs = _G.pairs
+	local select = _G.select
+	local SPELL_POWER_MANA = _G.SPELL_POWER_MANA
+	local UnitAura = _G.UnitAura
+	local UnitBuff = _G.UnitBuff
+	local UnitClass = _G.UnitClass
+	local UnitDebuff = _G.UnitDebuff
+	local UnitHealth = _G.UnitHealth
+	local UnitHealthMax = _G.UnitHealthMax
+	local UnitPower = _G.UnitPower
+	local UnitPowerMax = _G.UnitPowerMax
+
+	-- Mistweaver constants
+	local buff = GetSpellInfo(115151) -- Renewing Mist
+	local TFT_COUNT    = 4 -- Minimum number of Renewing Mist to highlight Thunder Focus Tea
+	local TFT_DURATION = 6 -- Duration threshold to highlight Thunder Focus Tea
+	local UPLIFT_THRESHOLD = 2 -- Heal multiplier to highlight Uplight
+
 	return {
 		ImportPlayerSpells {
 			-- Import all spells for ...
@@ -152,6 +178,67 @@ AdiButtonAuras:RegisterRules(function(addon)
 					end
 				end
 			end)()
+		},
+		Configure {
+			"RenewingMist",
+			addon.L["Show the number of group member affected by @NAME and the shortest duration."],
+			115151, -- Renewing Mist
+			"group",
+			"UNIT_AURA",
+			function(units, model)
+				local count, minExpiration = 0, math.huge
+				for unit in pairs(units.group) do
+					local name, _, _, _, _, _, expiration = UnitAura(unit, buff, nil, "HELPFUL PLAYER")
+					if name then
+						count, minExpiration = count + 1, min(minExpiration, expiration)
+					end
+				end
+				if count > 0 then
+					if count > 3 or GetNumGroupMembers() < 5 then
+						model.highlight = "good"
+					end
+					model.count, model.expiration = count, minExpiration
+				end
+			end
+		},
+		Configure {
+			"ThunderFocusTea",
+			format(addon.L["Highlight when at least %s %s are running and one of them is below %s seconds."], TFT_COUNT, buff, TFT_DURATION),
+			116680, -- Thunder Focus Tea
+			"group",
+			"UNIT_AURA",
+			function(units, model)
+				local count, minExpiration = 0, math.huge
+				for unit in pairs(units.group) do
+					local name, _, _, _, _, _, expiration = UnitAura(unit, buff, nil, "HELPFUL PLAYER")
+					if name then
+						count, minExpiration = count + 1, min(minExpiration, expiration)
+					end
+				end
+				if count >= TFT_COUNT and minExpiration-GetTime() < TFT_DURATION then
+					model.highlight, model.expiration = "flash", minExpiration
+				end
+			end
+		},
+		Configure {
+			"Uplift",
+			format(addon.L["Highlight when total effective healing would be at least %d times the base healing."], UPLIFT_THRESHOLD),
+			116670, -- Uplift
+			"group",
+			{ "UNIT_AURA", "UNIT_HEALTH", "UNIT_HEALTH_MAX" },
+			function(units, model)
+				-- Rough estimation at level 90
+				local heal = 1.2 * ((7210+8379)/2 + 0.68 * GetSpellBonusHealing())
+				local totalHeal = 0
+				for unit in pairs(units.group) do
+					if UnitAura(unit, buff, nil, "HELPFUL PLAYER") then
+						totalHeal = totalHeal + min(heal, UnitHealthMax(unit) - UnitHealth(unit))
+					end
+				end
+				if totalHeal >= UPLIFT_THRESHOLD * heal then
+					model.highlight = "flash"
+				end
+			end
 		},
 	}
 
