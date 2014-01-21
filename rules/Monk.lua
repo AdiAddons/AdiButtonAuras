4bef4299dd70694c288cdde8e84c47598170e6ab
Adirelle <adirelle@gmail.com>
2014-01-21 12:51:37 +0100

    monk: with the brewmaster passive "Desperate Measures", show hint Expel Harm when under 35% health.

--- rules/Monk.lua
+++ rules/Monk.lua
@@ -248,6 +248,19 @@ AdiButtonAuras:RegisterRules(function(addon)
 				end
 			end,
 		},
+		Configure {
+			"DesperateMeasures",
+			addon.L["Show hint when your health is below 35."],
+			115072, -- Expel Harm
+			"player",
+			{ "UNIT_HEALTH", "UNIT_HEALTH_MAX" },
+			function(_, model)
+				if UnitHealth("player") / UnitHealthMax("player") < 0.35 then
+					model.hint = true
+				end
+			end,
+			126060, -- Desperate Measures
+		},
 	}
 
 end)
