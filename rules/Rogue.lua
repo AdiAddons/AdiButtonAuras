cb89b9f35f635fb066cf54e69fa00587729359c3
Rainrider <rainrider.wow@gmail.com>
2014-02-03 03:50:27 +0100

    Rogue: add combo point display to finishers

--- rules/Rogue.lua
+++ rules/Rogue.lua
@@ -32,5 +32,43 @@ local addonName, addon = ...
 
 AdiButtonAuras:RegisterRules(function(addon)
 	addon.Debug('Rules', 'Adding rogue rules')
-	return ImportPlayerSpells { "ROGUE" }
+		local L = addon.L
+
+	return {
+		ImportPlayerSpells { "ROGUE" },
+		Configure {
+			"ComboPoints",
+			format(
+				addon.L["Show %s and %s when it reaches its maximum."],
+				addon.L["combo points"],
+				addon.DescribeHighlight("flash")
+			),
+			{
+				 32645, -- Envenom
+				  2098, -- Eviscerate
+				  5171, -- Slice and Dice
+				121411, -- Crimson Tempest
+				  1943, -- Rupture
+				   408, -- Kidney Shot
+				 73651, -- Recuperate
+				 26679, -- Deadly Throw
+
+				-- 73981, -- Redirect
+			},
+			{ "enemy", "player" },
+			"UNIT_COMBO_POINTS",
+			function(units, model)
+				if not units.enemy then return end
+				local points = GetComboPoints("player", units.enemy)
+				if points and points > 0 then
+					model.count = points or 0
+					if points == 5 then
+						model.highlight = "flash"
+					end
+					return true
+				end
+			end,
+		},
+	}
+
 end)
