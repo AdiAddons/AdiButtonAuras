457aa0bcdece2ee0c3ed88807e09459c0c4f08df
Adirelle <adirelle@gmail.com>
2014-02-15 01:20:33 +0100

    Warlock: actually use LibPlayerSpells.

--- rules/Warlock.lua
+++ rules/Warlock.lua
@@ -31,7 +31,18 @@ if select(2, UnitClass("player")) ~= "WARLOCK" then return end
 AdiButtonAuras:RegisterRules(function(addon)
 	addon.Debug('Rules', 'Adding warlock rules')
 
+	local _G = _G
+	local format = _G.format
+	local GetSpellInfo = _G.GetSpellInfo
+	local UnitAura = _G.UnitAura
+
 	return {
+		ImportPlayerSpells {
+			-- Import all spells for ...
+			"WARLOCK",
+			-- ... but ...
+			116858, -- Chaos Bolt
+		},
 		ShowPower {
 			{
 				17877,  -- Shadowburn
@@ -44,84 +55,6 @@ AdiButtonAuras:RegisterRules(function(addon)
 		ShowPower {
 			74434, -- Soulburn
 			"SOUL_SHARDS",
-		},
-		SelfBuffs {
-			  6229, -- Twilight Ward
-			  7812, -- Sacrifice (voidwalker buff)
-			 48018, -- Demonic Circle: Summon
-			 80240, -- Havoc
-			 91713, -- Nether Ward (talent)
-			104025, -- Immolation Aura (demon form)
-			104773, -- Unending Resolve
-			108416, -- Sacrificial Pact (talent)
-			108503, -- Grimoire of Sacrifice (talent)
-			108508, -- Mannoroth's Fury (talent)
-			108559, -- Demonic Rebirth
-			110913, -- Dark Bargain (talent)
-			111397, -- Blood Horror (talent)
-			113858, -- Dark Soul: Instability
-			113860, -- Dark Soul: Misery
-			113861, -- Dark Soul: Knowledge
-			114635, -- Ember Tap
-			116198, -- Aura of Enfeeblement (demon form)
-			119839, -- Fury Ward (Dark Apotheosis)
-			120451, -- Flames of Xoroth
-			132413, -- Shadow Bulwark (Grimoire of Sacrifice)
-		},
-		SharedSimpleBuffs {
-			  5697, -- Unending Breath
-			 20707, -- Soulstone
-		},
-		PetBuffs {
-			   755, -- Health Funnel
-			  1098, -- Enslave Demon
-		},
-		SimpleDebuffs {
-			   172, -- Corruption
-			   603, -- Metamorphosis: Doom
-			   980, -- Agony
-			 27243, -- Seed of Corruption
-			 30108, -- Unstable Affliction
-			 48181, -- Haunt
-		},
-		DebuffAliases {
-			{
-				   348, -- Immolate
-				108686, -- Immolate (Fire and Brimstone)
-			},
-			348, -- Immolate
-		},
-		DebuffAliases {
-			{
-				 17962, -- Conflagrate
-				108685, -- Conflagrate (Fire and Brimstone)
-			},
-			17962, -- Conflagrate
-		},
-		PassiveModifier {
-			117896, -- Backdraft
-			{
-				 29722, -- Incinerate
-				114654, -- Incinerate (Fire and Brimstone)
-			},
-			117828, -- Backdraft (buff)
-		},
-		--[[ Check if it already used or not
-		PassiveModifier {
-			108563, -- Backlash
-			 29722, -- Incinerate
-			108563, -- Backlash
-			"player",
-		},
-		]]
-		PassiveModifier {
-			122351, -- Molten Core
-			  6353, -- Soul Fire
-			122351, -- Molten Core
-		},
-		DebuffAliases {
-			105174, -- Hand of Gul'dan
-			 47960, -- Shadowflame
 		},
 		Configure {
 			"Pyroclasm",
