c8aee583e8f342092639adeb4856bab7864ceefd
Adirelle <adirelle@gmail.com>
2014-08-31 22:24:06 +0200

    Updated the localization.

--- Localization.lua
+++ Localization.lua
@@ -48,11 +48,23 @@ addon.L = L
 ------------------------ enUS ------------------------
 
 
+-- AdiButtonAuras.lua
+L["Could not load configuration panel"] = true
+
 -- Config.lua
+L["AdiButtonAuras has no rule for this spell/item."] = true
+L["AdiButtonAuras has special rules to suggest using a spell. How would like it to be display ?"] = true
 L["Check so actions on cooldown do not flash."] = true
+L["Check to disable flashs out of combat."] = true
 L["Check to flash instead of displaying a border."] = true
 L["Check to show a border when the (de)buff is missing."] = true
+L["Color of the countdown text for values above 3."] = true
+L["Color of the countdown text for values around 0."] = true
+L["Color of the countdown text for values around 3."] = true
 L["Configure spells and items individually."] = true
+L["Countdown above 10"] = true
+L["Countdown around 0"] = true
+L["Countdown around 3"] = true
 L["Countdown thresholds"] = true
 L["Disabled"] = true
 L["Duration above this threshold will use this format."] = true
@@ -60,6 +72,7 @@ L["Duration below this threshold will show decimals. Set to 0 to disable it."] =
 L["Durations above this threshold are hidden. Set to 0 to disable all countdowns."] = true
 L["Enabled"] = true
 L["Flash instead of border"] = true
+L["Flashing border"] = true
 L["Global"] = true
 L["Inverted"] = true
 L["Maximum duration for the \"2.7\" format"] = true
@@ -67,20 +80,27 @@ L["Maximum duration to show"] = true
 L["Minimum duration for the \"2m\" format"] = true
 L["Minimum duration for the \"4:58\" format"] = true
 L["No flash on cooldown"] = true
+L["No flash out of combat"] = true
 L["No selection"] = true
+L["Nothing"] = true
 L["Profiles"] = true
+L["Rotary star"] = true
 L["Rules"] = true
+L["Rules:"] = true
 L["Select a spell or item by clicking on a green or blue button. Darkened buttons indicate spells and items unknown to AdiButtonAuras."] = true
 L["Select which rules should by applied."] = true
 L["Shift+click to toggle."] = true
 L["Spells & items"] = true
 L["Status"] = true
+L["Suggestion display"] = true
 L["THIS DOES NOT AFFECT BLIZZARD FLASHS."] = true
 L["The color used for bad things, usually debuffs."] = true
 L["The color used for good things, usually buffs."] = true
 L["Uncheck to ignore this spell/item."] = true
 L["\"Bad\" border"] = true
 L["\"Good\" border"] = true
+L["item"] = true
+L["spell"] = true
 
 -- RuleDSL.lua
 L["%s when %s %s is found on %s."] = true
@@ -95,6 +115,7 @@ L["lighten"] = true
 L["show duration and/or stack count"] = true
 L["show the \"bad\" border"] = true
 L["show the \"good\" border"] = true
+L["suggest"] = true
 L["the buff"] = true
 L["the debuff"] = true
 L["the group members"] = true
@@ -105,27 +126,48 @@ L["your debuff"] = true
 L["your pet"] = true
 L["yourself"] = true
 
--- plugins/Mistweaver.lua
-L["Highlight when at least %s %s are running and one of them is below %s seconds."] = true
-L["Highlight when total effective healing would be at least %d times the base healing."] = true
-L["Show the number of group member affected by @NAME and the shortest duration."] = true
-
 -- rules/Common.lua
 L["%s when %s is casting/channeling a spell that you can interrupt."] = true
 L["Show when @NAME or an equivalent haste buff is found on yourself."] = true
+L["Track @NAME or equivalent raid buffs on all group members. Indicate the duration of the shortest buff and the number of missing buffs."] = true
 L["a buff you can dispel"] = true
 L["a debuff you can dispel"] = true
+L["a debuff"] = true
+L["of type '%s'"] = true
+
+-- rules/Deathknight.lua
+L["Shows Hint when target is below 35% health."] = true
 
 -- rules/Druid.lua
-L["Flash when mastery is inactive."] = true
+L["Suggests to cast Rejuvenation to enable Glyph of Rejuvenation effect."] = true
+L["Suggests when mastery is inactive."] = true
 L["combo points"] = true
 L["lunar energy"] = true
 L["solar energy"] = true
 
 -- rules/Monk.lua
+L["Show %s count and suggest using it at 10 or more stacks."] = true
+L["Show good border and remaining time of your summoned statue."] = true
+L["Show hint when your health is below 35%."] = true
+L["Show the number of group member affected by @NAME and the shortest duration."] = true
+L["Suggest using @NAME under 92% mana."] = true
+L["Suggest when at least %s %s are running and one of them is below %s seconds."] = true
+L["Suggest when total effective healing would be at least %d times the base healing."] = true
 L["stagger level"] = true
 
+-- rules/Priest.lua
+L["Show Power Word: Shield or Weakened Soul on targeted ally."] = true
+
+-- rules/Shaman.lua
+L["Show %s duration."] = true
+L["Show %s stacks."] = true
+L["air totems"] = true
+L["earth totems"] = true
+L["fire totems"] = true
+L["water totems"] = true
+
 -- rules/Warlock.lua
+L["%s Else %s"] = true
 L["%s when you have 3 or more stacks of %s."] = true
 
 
@@ -149,11 +191,8 @@ L["Durations above this threshold are hidden. Set to 0 to disable all countdowns
 L["Enabled"] = "Activé"
 L["flash"] = "flasher" -- Needs review
 L["Flash instead of border"] = "Flash au lieu du pourtour" -- Needs review
-L["Flash when mastery is inactive."] = "Flasher quand la maîtrise est inactive." -- Needs review
 L["Global"] = "Global"
 L["\"Good\" border"] = "\"Bon\" pourtour" -- Needs review
-L["Highlight when at least %s %s are running and one of them is below %s seconds."] = "Surligner quand au moins %s %s sont en cours et que l'une d'elle est en dessous de %s secondes." -- Needs review
-L["Highlight when total effective healing would be at least %d times the base healing."] = "Surligner quand le soin total effectif dépassera %d fois le soin de base." -- Needs review
 L["Inverted"] = "Inversé" -- Needs review
 L["it is above %s"] = "c'est au-dessus de %s" -- Needs review
 L["it is below %s"] = "c'est au-dessous de %s" -- Needs review
@@ -203,13 +242,139 @@ L["yourself"] = "vous-même" -- Needs review
 -- no translation
 
 ------------------------ esMX ------------------------
--- no translation
+elseif locale == 'esMX' then
+L["a buff you can dispel"] = "un benefico que puedes disipar"
+L["a debuff you can dispel"] = "un perjuicio que puedes disipar"
+L["\"Bad\" border"] = "Borde 'malo'"
+L["Check so actions on cooldown do not flash."] = "No destellar las acciones en cooldown."
+L["Check to flash instead of displaying a border."] = "Destellar en vez de mostrar un borde."
+L["Check to show a border when the (de)buff is missing."] = "Mostrar un borde cuando el beneficio o perjuicio se falta."
+L["combo points"] = "puntos de combo"
+L["Configure spells and items individually."] = "Configurar los hechizos y objectos de forma individual."
+L["Countdown thresholds"] = "Umbrales de cuentra atrás"
+L["darken"] = "oscurecerse"
+L["Disabled"] = "Desactivado"
+L["Duration above this threshold will use this format."] = "Duraciones más de este umbral se utilizan este formato."
+L["Duration below this threshold will show decimals. Set to 0 to disable it."] = "Duraciones menos de este umbral se muestran décimas. Establece a 0 para desactivar."
+L["Durations above this threshold are hidden. Set to 0 to disable all countdowns."] = "Duraciones más de este umbral se ocultan. Establece a 0 para desactivar todos cooldowns."
+L["Enabled"] = "Activado"
+L["flash"] = "destello"
+L["Flash instead of border"] = "Destello en vez de borde"
+L["Global"] = "Global"
+L["\"Good\" border"] = "Borde 'bueno'"
+L["Inverted"] = "Invertido"
+L["it is above %s"] = "está mas de %s"
+L["it is below %s"] = "está menos de %s"
+L["lighten"] = "aclarar"
+L["lunar energy"] = "energía lunar"
+L["Maximum duration for the \"2.7\" format"] = "Duración máxima para el formato '2.7'"
+L["Maximum duration to show"] = "Duración máxima para mostrar"
+L["Minimum duration for the \"2m\" format"] = "Duración mínima para el formato '2m'"
+L["Minimum duration for the \"4:58\" format"] = "Duración mínima para el formato '4:58'"
+L["No flash on cooldown"] = "No destellar en cooldown"
+L["No selection"] = "No hay selección"
+L["Profiles"] = "Perfiles"
+L["Rules"] = "Reglas"
+L["Select a spell or item by clicking on a green or blue button. Darkened buttons indicate spells and items unknown to AdiButtonAuras."] = "Seleccione un hechizo o objeto haciendo clic en un botón verde o azul. Los botones oscurecidos inidcan los hechizos y objetos que son desconocidos para AdiButtonAuras."
+L["Select which rules should by applied."] = "Seleccione cual reglas para applicar."
+L["Shift+click to toggle."] = "Mayús-clic para alternar."
+L["show duration and/or stack count"] = "mostrar duración y/o cuenta del montón"
+L["Show %s."] = "Mostrar %s."
+L["Show %s and %s when it reaches its maximum."] = "Mostrar %s y %s cuando se alcanza su máximo"
+L["Show %s and %s when %s."] = "Mostrar %s y %s cuando %s."
+L["show the \"bad\" border"] = "mostrar el borde 'malo'"
+L["show the \"good\" border"] = "mostrar el borde 'bueno'"
+L["Show the number of group member affected by @NAME and the shortest duration."] = "Mostrar el número del miembro del grupo quién está afectado por @NAME y la duración más corta."
+L["Show when @NAME or an equivalent haste buff is found on yourself."] = "Mostrar cuando @NAME o un beneficio equivalente se encuentra en ti mismo."
+L["solar energy"] = "energía solar"
+L["Spells & items"] = "Hechizos y objetos"
+L["stagger level"] = "nivel de Alpazar"
+L["Status"] = "Estado"
+L["%s when %s is casting/channeling a spell that you can interrupt."] = "%s cuando %s está lanzado o canalizando un hechizo que puedes interrumpir."
+L["%s when %s %s is found on %s."] = "%s cuando %s %s se encuentra en %s."
+L["%s when you have 3 or more stacks of %s."] = "%s cuando tienes 3 o más montones de %s."
+L["the buff"] = "el beneficio"
+L["The color used for bad things, usually debuffs."] = "El color a utilizar para las cosas malas, en general perjuicios."
+L["The color used for good things, usually buffs."] = "El color a utilizar para las cosas malas, en general beneficios."
+L["the debuff"] = "el perjuicio"
+L["the group members"] = "los miembres del grupo"
+L["the targeted ally"] = "el aliado seleccionado"
+L["the targeted enemy"] = "el aliado seleccionado"
+L["THIS DOES NOT AFFECT BLIZZARD FLASHS."] = "ESTO NO AFECTA LOS DESTELLOS DE BLIZZARD."
+L["Uncheck to ignore this spell/item."] = "Deseleccione para ignorar este hechizo o objecto."
+L["your buff"] = "tu beneficio"
+L["your debuff"] = "tu perjuicio"
+L["your pet"] = "tu mascota"
+L["yourself"] = "tú mismo"
 
 ------------------------ ruRU ------------------------
 -- no translation
 
 ------------------------ esES ------------------------
--- no translation
+elseif locale == 'esES' then
+L["a buff you can dispel"] = "un benefico que puedes disipar"
+L["a debuff you can dispel"] = "un perjuicio que puedes disipar"
+L["\"Bad\" border"] = "Borde 'malo'"
+L["Check so actions on cooldown do not flash."] = "No destellar las acciones en cooldown."
+L["Check to flash instead of displaying a border."] = "Destellar en vez de mostrar un borde."
+L["Check to show a border when the (de)buff is missing."] = "Mostrar un borde cuando el beneficio o perjuicio se falta."
+L["combo points"] = "puntos de combo"
+L["Configure spells and items individually."] = "Configurar los hechizos y objectos de forma individual."
+L["Countdown thresholds"] = "Umbrales de cuentra atrás"
+L["darken"] = "oscurecerse"
+L["Disabled"] = "Desactivado"
+L["Duration above this threshold will use this format."] = "Duraciones más de este umbral se utilizan este formato."
+L["Duration below this threshold will show decimals. Set to 0 to disable it."] = "Duraciones menos de este umbral se muestran décimas. Establece a 0 para desactivar."
+L["Durations above this threshold are hidden. Set to 0 to disable all countdowns."] = "Duraciones más de este umbral se ocultan. Establece a 0 para desactivar todos cooldowns."
+L["Enabled"] = "Activado"
+L["flash"] = "destello"
+L["Flash instead of border"] = "Destello en vez de borde"
+L["Global"] = "Global"
+L["\"Good\" border"] = "Borde 'bueno'"
+L["Inverted"] = "Invertido"
+L["it is above %s"] = "está mas de %s"
+L["it is below %s"] = "está menos de %s"
+L["lighten"] = "aclarar"
+L["lunar energy"] = "energía lunar"
+L["Maximum duration for the \"2.7\" format"] = "Duración máxima para el formato '2.7'"
+L["Maximum duration to show"] = "Duración máxima para mostrar"
+L["Minimum duration for the \"2m\" format"] = "Duración mínima para el formato '2m'"
+L["Minimum duration for the \"4:58\" format"] = "Duración mínima para el formato '4:58'"
+L["No flash on cooldown"] = "No destellar en cooldown"
+L["No selection"] = "No hay selección"
+L["Profiles"] = "Perfiles"
+L["Rules"] = "Reglas"
+L["Select a spell or item by clicking on a green or blue button. Darkened buttons indicate spells and items unknown to AdiButtonAuras."] = "Seleccione un hechizo o objeto haciendo clic en un botón verde o azul. Los botones oscurecidos inidcan los hechizos y objetos que son desconocidos para AdiButtonAuras."
+L["Select which rules should by applied."] = "Seleccione cual reglas para applicar."
+L["Shift+click to toggle."] = "Mayús-clic para alternar."
+L["show duration and/or stack count"] = "mostrar duración y/o cuenta del montón"
+L["Show %s."] = "Mostrar %s."
+L["Show %s and %s when it reaches its maximum."] = "Mostrar %s y %s cuando se alcanza su máximo"
+L["Show %s and %s when %s."] = "Mostrar %s y %s cuando %s."
+L["show the \"bad\" border"] = "mostrar el borde 'malo'"
+L["show the \"good\" border"] = "mostrar el borde 'bueno'"
+L["Show the number of group member affected by @NAME and the shortest duration."] = "Mostrar el número del miembro del grupo quién está afectado por @NAME y la duración más corta."
+L["Show when @NAME or an equivalent haste buff is found on yourself."] = "Mostrar cuando @NAME o un beneficio equivalente se encuentra en ti mismo."
+L["solar energy"] = "energía solar"
+L["Spells & items"] = "Hechizos y objetos"
+L["stagger level"] = "nivel de Alpazar"
+L["Status"] = "Estado"
+L["%s when %s is casting/channeling a spell that you can interrupt."] = "%s cuando %s está lanzado o canalizando un hechizo que puedes interrumpir."
+L["%s when %s %s is found on %s."] = "%s cuando %s %s se encuentra en %s."
+L["%s when you have 3 or more stacks of %s."] = "%s cuando tienes 3 o más montones de %s."
+L["the buff"] = "el beneficio"
+L["The color used for bad things, usually debuffs."] = "El color a utilizar para las cosas malas, en general perjuicios."
+L["The color used for good things, usually buffs."] = "El color a utilizar para las cosas malas, en general beneficios."
+L["the debuff"] = "el perjuicio"
+L["the group members"] = "los miembres del grupo"
+L["the targeted ally"] = "el aliado seleccionado"
+L["the targeted enemy"] = "el aliado seleccionado"
+L["THIS DOES NOT AFFECT BLIZZARD FLASHS."] = "ESTO NO AFECTA LOS DESTELLOS DE BLIZZARD."
+L["Uncheck to ignore this spell/item."] = "Deseleccione para ignorar este hechizo o objecto."
+L["your buff"] = "tu beneficio"
+L["your debuff"] = "tu perjuicio"
+L["your pet"] = "tu mascota"
+L["yourself"] = "tú mismo"
 
 ------------------------ zhTW ------------------------
 -- no translation
