5e63dbe4085ce4c5a1f3938f05a651e6c14b34d4
Rainrider <Rainrider@users.noreply.github.com>
2016-05-16 16:34:17 +0200

    Add the demon hunter class

---
+++ rules/Demonhunter.lua
@@ -0,0 +1,42 @@
+--[[
+AdiButtonAuras - Display auras on action buttons.
+Copyright 2013-2014 Adirelle (adirelle@gmail.com)
+All rights reserved.
+
+This file is part of AdiButtonAuras.
+
+AdiButtonAuras is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+AdiButtonAuras is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with AdiButtonAuras.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+
+local _, addon = ...
+
+if not addon.isClass("DEMONHUNTER") then return end
+
+AdiButtonAuras:RegisterRules(function()
+	Debug('Adding demonhunter rules')
+	return ImportPlayerSpells { "DEMONHUNTER" }
+end)
+
+-- GLOBALS: AddRuleFor BuffAliases BuildAuraHandler_FirstOf BuildAuraHandler_Longest
+-- GLOBALS: BuildAuraHandler_Single BuildDesc BuildKey Configure DebuffAliases Debug
+-- GLOBALS: DescribeAllSpells DescribeAllTokens DescribeFilter DescribeHighlight
+-- GLOBALS: DescribeLPSSource GetComboPoints GetEclipseDirection GetNumGroupMembers
+-- GLOBALS: GetShapeshiftFormID GetSpellBonusHealing GetSpellInfo GetTime
+-- GLOBALS: GetTotemInfo HasPetSpells ImportPlayerSpells L LongestDebuffOf
+-- GLOBALS: PLAYER_CLASS PassiveModifier PetBuffs SelfBuffAliases SelfBuffs
+-- GLOBALS: SharedSimpleBuffs SharedSimpleDebuffs ShowPower SimpleBuffs
+-- GLOBALS: SimpleDebuffs UnitCanAttack UnitCastingInfo UnitChannelInfo UnitClass
+-- GLOBALS: UnitHealth UnitHealthMax UnitIsDeadOrGhost UnitIsPlayer UnitPower
+-- GLOBALS: UnitPowerMax UnitStagger bit ceil floor format ipairs math min pairs
+-- GLOBALS: print select string table tinsert GetPlayerBuff ShowStacks
