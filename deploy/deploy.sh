232a2b12792488ac4e9e5b010aa47e4549d9bf84
Adirelle <adirelle+github@gmail.com>
2016-07-23 17:24:41 +0200

    Fixed deploy.sh.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 WOWACE_PROJECT="$1"
+ENCRYPT_KEY="$2"
+ENCRYPT_IV="$3"
 DEPLOY_DIR="$TRAVIS_BUILD_DIR/deploy"
 
 # Setup SSH
 mkdir -p "$HOME/.ssh"
 cp "$DEPLOY_DIR/wowace.hostkeys" "$HOME/.ssh/known_hosts"
-openssl aes-256-cbc -K "$encrypted_bfcd8b8ecc98_key" -iv "$encrypted_bfcd8b8ecc98_iv" -in "$DEPLOY_DIR/wowace.key.enc" -out "$HOME/.ssh/id_rsa" -d
+openssl aes-256-cbc -K "$ENCRYPT_KEY" -iv "$ENCRYPT_IV" -in "$DEPLOY_DIR/wowace.key.enc" -out "$HOME/.ssh/id_rsa" -d
 chmod -R og= "$HOME/.ssh"
 
 # Do push
