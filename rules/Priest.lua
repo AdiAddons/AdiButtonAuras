e4dbb14d44203842bf7030b8e3e7ccf41d07964f
Rainrider <Rainrider@users.noreply.github.com>
2016-08-02 16:35:04 +0200

    priest: add GetPlayerDebuff to the list of globals
    This is really annoying

--- rules/Priest.lua
+++ rules/Priest.lua
@@ -120,4 +120,4 @@ end)
 -- GLOBALS: SimpleDebuffs UnitCanAttack UnitCastingInfo UnitChannelInfo UnitClass
 -- GLOBALS: UnitHealth UnitHealthMax UnitIsDeadOrGhost UnitIsPlayer UnitPower
 -- GLOBALS: UnitPowerMax UnitStagger bit ceil floor format ipairs math min pairs
--- GLOBALS: print select string table tinsert GetPlayerBuff ShowStacks
+-- GLOBALS: print select string table tinsert GetPlayerBuff GetPlayerDebuff ShowStacks
