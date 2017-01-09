a552bca6f11d09f124a8d5b9f3e021ef9525d3d3
Rainrider <rainrider.wow@gmail.com>
2017-01-09 22:08:54 +0100

    Use https to push to wowace
    They removed ssh support.
    Be careful with changes to deploy.sh as it might expose private
    information.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 WOWACE_PROJECT="$1"
-ENCRYPT_KEY="$2"
-ENCRYPT_IV="$3"
-DEPLOY_DIR="$TRAVIS_BUILD_DIR/deploy"
-
-# Setup SSH
-mkdir -p "$HOME/.ssh"
-cp "$DEPLOY_DIR/wowace.hostkeys" "$HOME/.ssh/known_hosts"
-openssl aes-256-cbc -K "$ENCRYPT_KEY" -iv "$ENCRYPT_IV" -in "$DEPLOY_DIR/wowace.key.enc" -out "$HOME/.ssh/id_rsa" -d
-chmod -R og= "$HOME/.ssh"
 
 # Do push
-git push "https://repos.wowace.com/wow/${WOWACE_PROJECT}" "${TRAVIS_TAG:-$TRAVIS_BRANCH}"
+# DO NOT REMOVE the quiet flag and the output redirection
+git push -q "https://${WOWACE_USER}:${WOWACE_PASS}@repos.wowace.com/wow/${WOWACE_PROJECT}" "${TRAVIS_TAG:-$TRAVIS_BRANCH}" > /dev/null 2>&1
