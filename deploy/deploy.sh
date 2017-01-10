e2bec70c63e4eb758ec143f18a0becc8447e5d29
Adirelle <adirelle+github@gmail.com>
2017-01-10 20:57:16 +0100

    Updated the deploy process to use the new wowace webhook.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,2 @@
-#!/bin/bash
-WOWACE_PROJECT="$1"
-
-# Do push
-# DO NOT REMOVE the quiet flag and the output redirection
-git push -q "https://${WOWACE_USER}:${WOWACE_PASS}@repos.wowace.com/wow/${WOWACE_PROJECT}" "${TRAVIS_TAG:-$TRAVIS_BRANCH}" > /dev/null 2>&1
+#!/bin/sh
+exec curl -XPOST -sS "https://www.wowace.com/api/projects/$1/package?token=$WOWACE_API_TOKEN" -d '{"ping":1}'
