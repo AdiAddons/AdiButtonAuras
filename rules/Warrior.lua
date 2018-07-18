cd4f0bf8d5fee79a54b78ce4def3333c3c775b0e
Rainrider <Rainrider@users.noreply.github.com>
2018-07-19 01:38:02 +0200

    warrior: update for Battle for Azeroth (#291)

--- rules/Warrior.lua
+++ rules/Warrior.lua
@@ -16,15 +16,45 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with AdiButtonAuras.  If not, see <http://www.gnu.org/licenses/>.
+along with AdiButtonAuras. If not, see <http://www.gnu.org/licenses/>.
 --]]
 
 local _, addon = ...
 
-if not addon.isClass("WARRIOR") then return end
+if not addon.isClass('WARRIOR') then return end
 
 AdiButtonAuras:RegisterRules(function()
 	Debug('Adding warrior rules')
 
-	return ImportPlayerSpells { "WARRIOR" }
+	return {
+		ImportPlayerSpells {
+			-- add all spells for
+			'WARRIOR',
+			-- except for
+			115767, -- Deep Wounds (Protection)
+			236321, -- War Banner (Arms honor talent)
+			262115, -- Deep Wounds (Arms)
+		},
+
+		DebuffAliases {
+			 20243, -- Devastate (Protection)
+			115767, -- Deep Wounds
+			115768, -- Deep Wounds (Protection)
+		},
+
+		Configure {
+			'WarBanner',
+			L['Show the duration of %NAME.'],
+			236320, -- War Banner (Arms honor talent)
+			'player',
+			{ 'PLAYER_TOTEM_UPDATE', 'UNIT_AURA' },
+			function(_, model)
+				local found, _, start, duration = GetTotemInfo(1)
+				if found then
+					model.expiration = start + duration
+					model.highlight = GetPlayerBuff('player', 236321) and 'good' or nil
+				end
+			end,
+		},
+	}
 end)
