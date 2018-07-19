e6730dcb6479d54efdb2b00fd76d6adc0e452fdb
Rainrider <Rainrider@users.noreply.github.com>
2018-07-19 02:03:38 +0200

    Bump toc and copyleft

--- tests/Items.lua
+++ tests/Items.lua
@@ -1,6 +1,6 @@
 --[[
 AdiButtonAuras - Display auras on action buttons.
-Copyright 2013-2016 Adirelle (adirelle@gmail.com)
+Copyright 2013-2018 Adirelle (adirelle@gmail.com)
 All rights reserved.
 
 This file is part of AdiButtonAuras.
@@ -16,7 +16,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with AdiButtonAuras.  If not, see <http://www.gnu.org/licenses/>.
+along with AdiButtonAuras. If not, see <http://www.gnu.org/licenses/>.
 --]]
 
 package.path = package.path .. ";./wowmock/?.lua"
