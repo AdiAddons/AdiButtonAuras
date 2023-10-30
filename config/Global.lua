ee63ab9e511c990e9d0489112f395df9ec200ba9
Rainrider <Rainrider@users.noreply.github.com>
2023-10-30 18:44:34 +0100

    build: bump toc and copyright

--- config/Global.lua
+++ config/Global.lua
@@ -1,6 +1,6 @@
 --[[
 AdiButtonAuras - Display auras on action buttons.
-Copyright 2013-2022 Adirelle (adirelle@gmail.com)
+Copyright 2013-2023 Adirelle (adirelle@gmail.com)
 All rights reserved.
 
 This file is part of AdiButtonAuras.
